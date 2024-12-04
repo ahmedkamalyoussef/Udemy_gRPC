e1a2dfce7d2339ec018d2f10795eaf7f511be73f
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-12-04 11:33:49 +0200

    dummy proto & first connection

--- server/Program.cs
+++ server/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Grpc.Core;
+using System;
+using System.IO;
 
 namespace server
 {
     internal class Program
     {
+        const int port = 50051;
         static void Main(string[] args)
         {
+            Server s = null;
+            try
+            {
+                s = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+                s.Start();
+                Console.WriteLine("lesting on " + port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"{e.Message}");
+                throw;
+            }
+            finally
+            {
+                if (s != null)
+                    s.ShutdownAsync().Wait();
+
+
+            }
         }
     }
 }
